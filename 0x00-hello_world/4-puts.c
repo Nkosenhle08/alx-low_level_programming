16ead2ac500c31fa9505cc8cc81fe27122645159
Nkosenhle08 <axe.benefit123@gmail.com>
2023-08-10 13:54:18 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 after complete
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
