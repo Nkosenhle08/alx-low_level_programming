064f0899ea210eba24479eb56dc9aad79a26e58b
Nkosenhle08 <axe.benefit123@gmail.com>
2023-08-16 04:47:22 -0700

    x10 alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - tion that prints 10 times the alphabet
+ *
+ * Return: 0
+ */
+
+
+void print_alphabet_x10(void)
+{
+	char j;
+	int i;
+
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
